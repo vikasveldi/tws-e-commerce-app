14386b647b92b9e0158342535a2acf507fea7e34
Vikas Veldi <128746937+vikasveldi@users.noreply.github.com>
2026-05-18 18:35:56 +0530

    Update install_tools.sh

--- terraform/install_tools.sh
+++ terraform/install_tools.sh
@@ -49,13 +49,16 @@ sudo apt-get update -y
 sudo apt-get install -y trivy
 
 echo "========= Helm Setup ========="
-curl -fsSL https://baltocdn.com/helm/signing.asc | \
-gpg --dearmor | \
-sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+echo "========= Helm Setup ========="
+curl https://baltocdn.com/helm/signing.asc | \
+gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | \
 sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+
 sudo apt-get update -y
-sudo apt-get update -y
+sudo apt-get install -y helm
+
 
 echo "========= Installed Versions ========="
 echo "Java Version:"
