08b4c6e3b727d0eef3c71673c97753250b309dab
vikas veldi <vikasveldi@gmail.com>
2026-05-18 13:38:17 +0530

    Update Terraform modules to remove warnings

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,7 +1,7 @@
 module "eks" {
 
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.1"
+  version = "~> 20.0"
 
   cluster_name                   = local.name
   cluster_endpoint_public_access = true
